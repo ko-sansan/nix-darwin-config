51e12a5ec6bc8b10ad19962b68fed7b2a02758c8
Koichiro Okamoto <koichirookamoto@icloud.com>
2025-07-30 12:44:34 +0900

    Add neotest keymaps and a dependency

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -6,8 +6,29 @@ return {
 			"nvim-neotest/nvim-nio",
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-neotest/neotest-jest",
+			"nvim-neotest/neotest-go",
 			"olimorris/neotest-rspec",
 		},
+		config = function()
+			local neotest = require("neotest")
+
+			-- キーマッピング（例）
+			vim.keymap.set("n", "<leader>tt", function()
+				neotest.run.run()
+			end, { desc = "Run nearest test" })
+
+			vim.keymap.set("n", "<leader>tT", function()
+				neotest.run.run(vim.fn.expand("%"))
+			end, { desc = "Run all tests in file" })
+
+			vim.keymap.set("n", "<leader>to", function()
+				neotest.output.open({ enter = true })
+			end, { desc = "Show test output" })
+
+			vim.keymap.set("n", "<leader>ts", function()
+				neotest.summary.toggle()
+			end, { desc = "Toggle test summary" })
+		end,
 		opts = function(_, opts)
 			opts.adapters = {
 				require("neotest-jest")({
