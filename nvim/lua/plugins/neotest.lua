9db027441c260b45736a7fb315e6c02dcb70b764
Koichiro Okamoto <koichirookamoto@icloud.com>
2025-07-30 21:54:27 +0900

    Fix neotest config

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -12,6 +12,14 @@ return {
 		config = function()
 			local neotest = require("neotest")
 
+			neotest.setup({
+				adpaters = {
+					require("neotest-rspec"),
+					require("neotest-jest"),
+					require("neotest-go"),
+				},
+			})
+
 			-- キーマッピング（例）
 			vim.keymap.set("n", "<leader>tt", function()
 				neotest.run.run()
@@ -28,21 +36,6 @@ return {
 			vim.keymap.set("n", "<leader>ts", function()
 				neotest.summary.toggle()
 			end, { desc = "Toggle test summary" })
-		end,
-		opts = function(_, opts)
-			opts.adapters = {
-				require("neotest-jest")({
-					jestCommand = "npm test --", -- または "yarn test --"
-					jestConfigFile = "jest.config.js",
-					env = { CI = true },
-					cwd = vim.fn.getcwd,
-				}),
-				require("neotest-rspec")({
-					rspec_cmd = function()
-						return { "bundle", "exec", "rspec" }
-					end,
-				}),
-			}
 		end,
 	},
 }
